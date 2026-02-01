5ab9d2308c2ef315f0a0dd229cc3502084efbf29
rustam7733 <111507007+rustam7733@users.noreply.github.com>
2026-02-01 18:54:11 +0500

    Update Program.cs

--- task3/Program.cs
+++ task3/Program.cs
@@ -4,13 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/rustamsubhanov41_gmail_com", (string x, string y) => {
+    if (!BigInteger.TryParse(x, out var n1) || !BigInteger.TryParse(y, out var n2)) return "NaN";
     var num1 = BigInteger.Parse(x);
     var num2 = BigInteger.Parse(y);
-
-    if (!BigInteger.TryParse(x, out var n1) || !BigInteger.TryParse(y, out var n2)) return "NaN";
-
     if (num1 < 0 || num2 < 0) return "NaN";
-
     static BigInteger GetCommonMultiple(BigInteger a, BigInteger b)
     {
         a = BigInteger.Abs(a);
